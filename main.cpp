634e07264751faa03ca0f3e39da2618497cce1f5
Vadim <v.bodyansky@yandex.ru>
2020-06-07 19:46:56 +1000

    modifed example

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@
 void func1()
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	std::cout << std::this_thread::get_id() << "\t";
+	std::cout << "A";
+}
+
+void func2()
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	std::cout << "O";
 }
 
 
@@ -16,6 +22,7 @@ int main()
 	std::thread thr([&] {
 		for (int i = 0; i < 10; i++) {
 			pool.push_task(func1);
+			pool.push_task(func2);
 		}
 	});
 	thr.detach();
