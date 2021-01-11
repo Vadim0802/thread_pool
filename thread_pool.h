457b32b3b2ec35365267da0f2162f6196c8f9e42
bodia <50073316+Vadim0802@users.noreply.github.com>
2021-01-12 00:11:53 +1000

    ref

--- thread_pool.h
+++ thread_pool.h
@@ -11,29 +11,27 @@
 class ThreadPool
 {
 private:
-	class Task
-	{
+	class Task {
 	private:
 		std::string	          _statusWork;
-		std::function<void()> _job;
+		std::function<void()> 	  _job;
 	public:
-		Task(std::function<void()> job)
-		{
+		Task(std::function<void()> job) {
 			this->_job = job;
 			this->_statusWork = "inQueue";
 		}
+		
 		void operator()() { _job(); };
 		std::string getStatusWork() { return this->_statusWork; };
 		void setStatusWork(std::string status) { this->_statusWork = status; };
 	};
-	std::mutex	                       _mutex;
+	std::mutex	                   _mutex;
 	std::condition_variable	           _condition;
 	std::vector<std::thread>           _threads;
 	std::vector<std::shared_ptr<Task>> _workerQueue;
 
 public:
-	auto push_task(std::function<void()> job)
-	{
+	auto push_task(std::function<void()> job) {
 		std::unique_lock<std::mutex> lock(_mutex);
 		Task worker(job);
 		_workerQueue.push_back(std::make_shared<Task>(std::move(worker)));
@@ -45,23 +43,18 @@ public:
 
 	std::string getStatus(std::shared_ptr<Task> ptr) { return ptr->getStatusWork(); };
 
-	ThreadPool(unsigned int threadCount)
-	{
+	ThreadPool(unsigned int threadCount) {
 		int MAX_THREADS = std::thread::hardware_concurrency() - 1;
 		if (threadCount > MAX_THREADS)
 			threadCount = MAX_THREADS;
 
-		for (int i = 0; i < threadCount; i++)
-		{
+		for (int i = 0; i < threadCount; i++) {
 			_threads.emplace_back([=] {
-				while (true)
-				{
-					for (int j = 0; j < _workerQueue.size(); j++)
-					{
+				while (true) {
+					for (int j = 0; j < _workerQueue.size(); j++) {
 						auto Task = _workerQueue[j];
 
-						if (Task->getStatusWork() == "inQueue")
-						{
+						if (Task->getStatusWork() == "inQueue") {
 							_mutex.lock();
 							(*Task).setStatusWork("inThreading");
 							_mutex.unlock();
@@ -75,8 +68,7 @@ public:
 
 					bool threadEndWork = true;
 
-					for (auto iter : _workerQueue)
-					{
+					for (auto iter : _workerQueue) {
 						if (iter->getStatusWork() != "Completed")
 							threadEndWork = false;
 					}
@@ -86,10 +78,8 @@ public:
 			});
 		}
 	}
-	~ThreadPool()
-	{
-		for (auto& t : _threads)
-		{
+	~ThreadPool() {
+		for (auto& t : _threads) {
 			if (t.joinable())
 				t.join();
 		}
